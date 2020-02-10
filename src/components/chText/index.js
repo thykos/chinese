71f89ac1e4a37de6e2af6737c6bfa731107fca14
thykos <thykos1994@gmail.com>
2020-02-10 22:35:25 +0200

    play audio on click

--- src/components/chText/index.js
+++ src/components/chText/index.js
@@ -3,13 +3,21 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import './styles.css';
 
-const ChText = ({ children, size, className }) => (
-  <span className={cx('ch-chinese-text', className)} style={{ fontSize: size }}>{children}</span>
+const ChText = ({ children, size, className, animate }) => (
+  <span
+    className={
+      cx('ch-chinese-text', className, {'ch-chinese-text-animate': animate})
+    }
+    style={{ fontSize: size }}
+  >
+    {children}
+  </span>
 );
 
 ChText.propTypes = {
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.string]).isRequired,
   size: PropTypes.number,
+  animate: PropTypes.bool,
   className: PropTypes.string
 };
 
