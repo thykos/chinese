ece71f92c5c4724db1693ffc7f6193ccc2b99aa6
thykos <thykos1994@gmail.com>
2020-02-10 12:11:45 +0200

    update library

--- src/static/library.js
+++ src/static/library.js
@@ -318,5 +318,251 @@ export default [
     ru: 'Выйти',
     ch: '离开',
     pinyin: 'lí kāi'
+  },
+  {
+    ru: 'Говорить',
+    ch: '说',
+    pinyin: 'shuō'
+  },
+  {
+    ru: 'Смотреть',
+    ch: '看',
+    pinyin: 'kān'
+  },
+  {
+    ru: 'Кушать',
+    ch: '吃',
+    pinyin: 'chī'
+  },
+  {
+    ru: 'Пить',
+    ch: '喝',
+    pinyin: 'hē'
+  },
+  {
+    ru: 'Сидеть',
+    ch: '坐',
+    pinyin: 'zuò'
+  },
+  {
+    ru: 'Слушать',
+    ch: '听',
+    pinyin: 'tīng'
+  },
+  {
+    ru: 'Иметь',
+    ch: '有',
+    pinyin: 'yǒu'
+  },
+  {
+    ru: 'Новости',
+    ch: '新闻',
+    pinyin: 'xīn wén'
+  },
+  {
+    ru: 'Вопрос',
+    ch: '问题',
+    pinyin: 'wèn tí'
+  },
+  {
+    ru: 'Ноль',
+    ch: '零',
+    pinyin: 'líng'
+  },
+  {
+    ru: 'Институт',
+    ch: '学院',
+    pinyin: 'xué yuàn'
+  },
+  {
+    ru: 'Супермаркет',
+    ch: '超市',
+    pinyin: 'chāo shì'
+  },
+  {
+    ru: 'ТРЦ',
+    ch: '商场',
+    pinyin: 'shāng chǎng'
+  },
+  {
+    ru: 'Остановка метро',
+    ch: '地铁站',
+    pinyin: 'di tiě zhàn'
+  },
+  {
+    ru: 'Остановка автобуса(автовокзал)',
+    ch: '公交车站',
+    pinyin: 'gōng jiāo chē zhàn'
+  },
+  {
+    ru: 'Туалет',
+    ch: '卫生间 / 厕所',
+    pinyin: 'wèi shēng jiān / cè suǒ'
+  },
+  {
+    ru: 'Банк',
+    ch: '银行',
+    pinyin: 'yín háng'
+  },
+  {
+    ru: 'Кинотеатр',
+    ch: '电影院',
+    pinyin: 'diàn yǐng yuàn'
+  },
+  {
+    ru: 'Нравиться',
+    ch: '喜欢',
+    pinyin: 'xǐhuān'
+  },
+  {
+    ru: 'Любовь',
+    ch: '爱',
+    pinyin: 'ài'
+  },
+  {
+    ru: 'Компьютер',
+    ch: '电脑',
+    pinyin: 'diàn nǎo'
+  },
+  {
+    ru: 'Телевизор',
+    ch: '电视',
+    pinyin: 'diàn shì'
+  },
+  {
+    ru: 'Спасибо',
+    ch: '谢谢',
+    pinyin: 'xiè xie'
+  },
+  {
+    ru: 'Пожалуйста',
+    ch: '不客气',
+    pinyin: 'bù kèqì'
+  },
+  {
+    ru: 'Извините',
+    ch: '对不起',
+    pinyin: 'duìbùqǐ'
+  },
+  {
+    ru: 'Суши',
+    ch: '寿司',
+    pinyin: 'shòusī'
+  },
+  {
+    ru: 'Знать',
+    ch: '知道',
+    pinyin: 'zhīdào'
+  },
+  {
+    ru: 'Еще раз(снова)',
+    ch: '再来一次',
+    pinyin: 'zàilái yīcì'
+  },
+  {
+    ru: 'Дом',
+    ch: '家',
+    pinyin: 'jiā'
+  },
+  {
+    ru: 'Идти',
+    ch: '去',
+    pinyin: 'qù'
+  },
+  {
+    ru: 'Плавать',
+    ch: '游泳',
+    pinyin: 'yóu yǒng'
+  },
+  {
+    ru: 'Вчера',
+    ch: '昨天',
+    pinyin: 'zuó tiān'
+  },
+  {
+    ru: 'Пекинская опера',
+    ch: '京剧',
+    pinyin: 'jīng jù'
+  },
+  {
+    ru: 'Как / Какой',
+    ch: '怎么样',
+    pinyin: 'zěn me yàng'
+  },
+  {
+    ru: 'Интересный',
+    ch: '有意思',
+    pinyin: 'yǒuyìsi'
+  },
+  {
+    ru: 'Сегодня',
+    ch: '今天',
+    pinyin: 'jīntiān'
+  },
+  {
+    ru: 'День',
+    ch: '天',
+    pinyin: 'tiān'
+  },
+  {
+    ru: 'Погода',
+    ch: '天气',
+    pinyin: 'tiānqì'
+  },
+  {
+    ru: 'Слишком',
+    ch: '太',
+    pinyin: 'tài'
+  },
+  {
+    ru: 'Что',
+    ch: '什么',
+    pinyin: 'shén me'
+  },
+  {
+    ru: 'Скучный',
+    ch: '没意思',
+    pinyin: 'méiyìsi'
+  },
+  {
+    ru: 'Время',
+    ch: '时候',
+    pinyin: 'shíhòu'
+  },
+  {
+    ru: 'Сейчас',
+    ch: '现在',
+    pinyin: 'xiànzài'
+  },
+  {
+    ru: 'Раньше',
+    ch: '以前',
+    pinyin: 'yǐ qián'
+  },
+  {
+    ru: 'Позже',
+    ch: '以后',
+    pinyin: 'yǐ hòu'
+  },
+  {
+    ru: 'Холодно',
+    ch: '冷',
+    pinyin: 'lěng'
+  },
+  {
+    ru: 'Жарко',
+    ch: '热',
+    pinyin: 'rè'
+  },
+  {
+    ru: 'Настроение',
+    ch: '心情',
+    pinyin: 'xīnqíng'
+  },
+  {
+    ru: 'Завтра',
+    ch: '明天',
+    pinyin: 'míngtiān'
   }
+
 ];
